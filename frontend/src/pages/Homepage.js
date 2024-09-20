5bc16b44aaf59eb2da56bc4f673ba3da3c13294a
prabhleen-sky <prabhleen.kaur@droom.in>
2024-09-20 16:12:08 +0530

    used graphql

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import useFetch from '../hooks/useFetch'
+// import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const REVIEWS = gql`
+    query GetReviews{
+        reviews{
+            documentId,
+            title,
+            rating,
+            body
+        }
+    }
+  
+`
 
 export default function Homepage() {
-    const { loading, error, data } = useFetch('http://localhost:1337/api/reviews')
+    // const { loading, error, data } = useFetch('http://localhost:1337/api/reviews')
+    const { loading, error, data } = useQuery(REVIEWS)
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
@@ -12,7 +26,7 @@ export default function Homepage() {
 
   return (
     <div>
-         {data.data.map((review) => (
+         {data.reviews.map((review) => (
 
             <div key={review.documentId} className="review-card">
                 <div className="rating">{review.rating}</div>
--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+// import useFetch from '../hooks/useFetch'
+import { useQuery, gql } from '@apollo/client'
+
+const REVIEW = gql `
+    query GetReview($id: ID!){
+        review(documentId: $id) {
+            documentId,
+            title,
+            rating,
+            body,
+          }
+    }
+`
 
 export default function ReviewDetails() {
     const { id } = useParams()
-    const { loading, error, data } = useFetch('http://localhost:1337/api/reviews/' + id)
+    // const { loading, error, data } = useFetch('http://localhost:1337/api/reviews/' + id)
+
+    const { loading, error, data } = useQuery(REVIEW, {
+        variables : {id : id}
+    })
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
@@ -13,12 +29,12 @@ export default function ReviewDetails() {
 
   return (
     <div className="review-card">
-        <div className="rating">{data.data.rating}</div>
-        <h2>{data.data.title}</h2>
+        <div className="rating">{data.review.rating}</div>
+        <h2>{data.review.title}</h2>
         {/* <p>{data.data.body[0].children[0].text}</p>
          */}
           <p>
-            {data.data.body.map((paragraph, pIndex) =>
+            {data.review.body.map((paragraph, pIndex) =>
             paragraph.children.map((child, cIndex) => (
                 <span key={`${pIndex}-${cIndex}`}>
                 {child.text}{" "}
